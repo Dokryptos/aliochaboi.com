e35327a29ac5bac2469ea20a642fe89f2f0c7a32
Pierre Dok <pierredok@outlook.fr>
2025-02-17 21:57:00 +0100

    adding session storage for have only one Intro animation

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -6,41 +6,65 @@ import { useEffect, useState } from "react";
 export default function Intro() {
   const [isVisibleBg, setIsVisibleBg] = useState(true);
   const [isVisibleH1, setIsVisibleH1] = useState(true);
+  const [showIntro, setShowIntro] = useState(false);
 
   useEffect(() => {
-    const timerBg = setTimeout(() => {
+    if (typeof window !== "undefined") {
+      const hasSeenIntro = sessionStorage.getItem("hasSeenIntro");
+      if (!hasSeenIntro) {
+        setShowIntro(true);
+        sessionStorage.setItem("hasSeenIntro", "true");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const animationSeen = sessionStorage.getItem("animationSeen");
+
+    if (animationSeen === "true") {
+      // Si l'animation a déjà été vue, ne rien faire
       setIsVisibleBg(false);
-    }, 1500);
-
-    const timerH1 = setTimeout(() => {
       setIsVisibleH1(false);
-    }, 4000);
+    } else {
+      const timerBg = setTimeout(() => {
+        setIsVisibleBg(false);
+      }, 1500);
 
-    return () => {
-      clearTimeout(timerBg);
-      clearTimeout(timerH1);
-    };
+      const timerH1 = setTimeout(() => {
+        setIsVisibleH1(false);
+        sessionStorage.setItem("animationSeen", "true");
+      }, 4000);
+
+      return () => {
+        clearTimeout(timerBg);
+        clearTimeout(timerH1);
+      };
+    }
   }, []);
   return (
-    <div>
-      <motion.div
-        className={`fixed inset-0 z-30 bg-principal ${isVisibleBg ? "block" : "hidden"}`}
-        initial={{ y: 0 }}
-        animate={{ y: "-100%" }}
-        transition={{ duration: 0.5, delay: 1, ease: "easeOut" }}
-      ></motion.div>
-      <motion.div
-        className={`fixed z-40 mt-[80px] mb-[80px] inset-0 flex items-center justify-center ${isVisibleH1 ? "block" : "hidden"}`}
-      >
-        <motion.h1
-          className="font-neueGrotesk mix-blend-difference text-black dekstop:text-[70px] tablet:text-[45px] text-[35px]"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
-          transition={{ duration: 1.5, delay: 3, ease: "easeOut" }}
-        >
-          Aliocha Boi
-        </motion.h1>
-      </motion.div>
-    </div>
+    <>
+      {showIntro && (
+        <div>
+          <motion.div
+            className={`fixed inset-0 z-30 bg-principal ${isVisibleBg ? "block" : "hidden"}`}
+            initial={{ y: 0 }}
+            animate={{ y: "-100%" }}
+            transition={{ duration: 0.5, delay: 1, ease: "easeOut" }}
+          ></motion.div>
+          <motion.div
+            className={`fixed z-40 mt-[80px] mb-[80px] inset-0 flex items-center justify-center ${isVisibleH1 ? "block" : "hidden"}`}
+          >
+            <motion.h1
+              className="font-neueGrotesk mix-blend-difference text-black dekstop:text-[70px] tablet:text-[45px] text-[35px]"
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 0 }}
+              transition={{ duration: 1.5, delay: 3, ease: "easeOut" }}
+            >
+              Aliocha Boi
+            </motion.h1>
+          </motion.div>
+        </div>
+      )}
+    </>
   );
 }
