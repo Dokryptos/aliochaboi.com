79fa394f55c02cac90213bd265bef7d62dc915d4
Pierre Dok <pierredok@outlook.fr>
2025-03-24 20:21:53 +0100

    width image + high

--- components/layouts/navbar/index.tsx
+++ components/layouts/navbar/index.tsx
@@ -13,7 +13,7 @@ export default function LayoutNavbar() {
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full z-20 font-neueGrotesk text-[16px]/5 laptop:text-[20px]/[26px]   ${bgColor}`}
+      className={`fixed top-0 left-0 w-full z-20 font-neueGrotesk text-[15px]/5 laptop:text-[20px]/[26px]   ${bgColor}`}
     >
       <Grid className="gap-5">
         <div className="laptop:col-start-1 laptop:col-span-6 tablet:col-span-4 pl-5 tablet:flex hidden ">
